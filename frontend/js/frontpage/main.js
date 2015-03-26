216d412b3bfd09125ec129a39e523a0ec232006e
donkeysharp <serguimant@gmail.com>
2015-03-26 10:54:03 -0400

    Created a json rest abstraction, a simpler angular-like $http

--- frontend/js/frontpage/main.js
+++ frontend/js/frontpage/main.js
@@ -1,5 +1,43 @@
 (function($) {
-  $(document).ready(function(){
-    console.log('Frontpage :3');
-  });
+  function geyQueryString(data) {
+    if (!data) { return ''; }
+
+    var queryString = '?', first = true;
+    for(var key in data) {
+      if (first) {
+        first = false;
+      } else {
+        queryString += '&';
+      }
+      queryString += key + '=' + data[key];
+    }
+    return queryString;
+  }
+
+  function getHttpRequest(url, method, data) {
+    data = JSON.stringify(data);
+    return $.ajax(url, {
+      contentType: 'application/json',
+      data: data,
+      dataType: 'json',
+      type: method
+    });
+  }
+
+  var $http = {
+    get: function(url, data) {
+      url = url + geyQueryString(data);
+      return $.ajax(url);
+    },
+    post: function(url, data) {
+      return getHttpRequest(url, 'POST', data);
+    },
+    put: function(url, data) {
+      return getHttpRequest(url, 'PUT', data);
+    },
+    delete: function(url, data) {
+      return getHttpRequest(url, 'DELETE', data);
+    }
+  };
+  window.$http = $http;
 }).call(document, jQuery);
